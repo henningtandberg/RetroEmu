bc88f945e33558c4b981f8cc9936fd1eaf4186c0
Henning Tandberg <henning@cowork.no>
2024-03-10 18:46:05 +0100

    remove Execute method from instruction

--- src/RetroEmu.Devices/DMG/CPU/Instructions/IInstruction.cs
+++ src/RetroEmu.Devices/DMG/CPU/Instructions/IInstruction.cs
@@ -5,5 +5,4 @@ internal interface IInstruction
 	public WriteType WriteOp { get; }
 	public OpType Op { get; }
 	public FetchType FetchOp { get; }
-	public int Execute(Processor processor);
 }
--- src/RetroEmu.Devices/DMG/CPU/Instructions/Instruction.cs
+++ src/RetroEmu.Devices/DMG/CPU/Instructions/Instruction.cs
@@ -1,10 +1,3 @@
 namespace RetroEmu.Devices.DMG.CPU.Instructions;
 
-internal record Instruction(WriteType WriteOp, OpType Op, FetchType FetchOp) : IInstruction
-{
-	public int Execute(Processor processor) 
-	{
-		// TODO: Implement the contents of Processor.Update() here
-		throw new System.NotImplementedException();
-	}
-}
+internal record Instruction(WriteType WriteOp, OpType Op, FetchType FetchOp) : IInstruction;
