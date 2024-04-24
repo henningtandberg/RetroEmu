8f53e289556c21bcaaf4999fe363e63bc1356693
Eilif Swensen <eilifswe@gmail.com>
2024-04-24 20:31:14 +0200

    Add CB Bit instructions

--- src/RetroEmu.Devices/DMG/CPU/Processor.Cb.cs
+++ src/RetroEmu.Devices/DMG/CPU/Processor.Cb.cs
@@ -9,8 +9,34 @@ namespace RetroEmu.Devices.DMG.CPU
         internal (byte, ushort) PerformCBOperation(CBType cbType, ushort fetchValue)
         {
             return cbType switch {
+                CBType.BIT0 => Bit(fetchValue, 0),
+                CBType.BIT1 => Bit(fetchValue, 1),
+                CBType.BIT2 => Bit(fetchValue, 2),
+                CBType.BIT3 => Bit(fetchValue, 3),
+                CBType.BIT4 => Bit(fetchValue, 4),
+                CBType.BIT5 => Bit(fetchValue, 5),
+                CBType.BIT6 => Bit(fetchValue, 6),
+                CBType.BIT7 => Bit(fetchValue, 7),
                 _ => throw new NotImplementedException()
             };
+        }
+
+        private (byte, ushort) Bit(ushort fetchValue, byte bit)
+        {
+            byte b = (byte)((fetchValue >> bit) & 0x01);
+
+            if (b == 0)
+            {
+                SetFlag(Flag.Zero);
+            }
+            else
+            {
+                ClearFlag(Flag.Zero);
+            }
+            ClearFlag(Flag.Subtract);
+            SetFlag(Flag.HalfCarry);
+
+            return (4, fetchValue);
         }
     }
 }
