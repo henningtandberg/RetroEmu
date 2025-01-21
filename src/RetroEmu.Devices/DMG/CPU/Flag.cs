9be99379323e2cdeffb7a55aa02d8d8a2ad4b4e0
Eilif Swensen <eilifswe@gmail.com>
2025-01-21 19:16:52 +0100

    Fix super flag bug

--- src/RetroEmu.Devices/DMG/CPU/Flag.cs
+++ src/RetroEmu.Devices/DMG/CPU/Flag.cs
@@ -2,9 +2,9 @@ namespace RetroEmu.Devices.DMG.CPU
 {
     public enum Flag
     {
-        Carry = 0x80,
-        HalfCarry = 0x40,
-        Subtract = 0x20,
-        Zero = 0x10
+        Carry = 0x10,
+        HalfCarry = 0x20,
+        Subtract = 0x40,
+        Zero = 0x80
     }
 }
