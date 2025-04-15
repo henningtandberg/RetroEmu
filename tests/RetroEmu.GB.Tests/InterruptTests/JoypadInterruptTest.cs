080204679716ceeaf81d795373bab77ac6720ff5
Henning Tandberg <henningtandberg@gmail.com>
2025-04-15 20:40:38 +0200

    added case for all interrups generated by joypad input

--- tests/RetroEmu.GB.Tests/InterruptTests/JoypadInterruptTest.cs
+++ tests/RetroEmu.GB.Tests/InterruptTests/JoypadInterruptTest.cs
@@ -8,18 +8,42 @@ namespace RetroEmu.GB.Tests.InterruptTests;
 
 public class JoypadInterruptTest
 {
-    private const ushort CodeExecutionStartAddress = 0x0150;
     private const byte JoypadInterruptDidNotTriggerValue = 0x17;
     private const byte JoypadInterruptDidTriggerValue = 0xAA;
+
+    private const byte DPadEnabled = 0x20;
+    private const byte ButtonsEnabled = 0x10;
     
-    [Fact]
-    public static void InterruptProgram_ButtonAIsPressed_JoypadInterruptTriggered()
+    private const byte DPadRightOrButtonA = 0x00;
+    private const byte DPadLeftOrButtonB = 0x01;
+    private const byte DPadUpOrButtonSelect = 0x02;
+    private const byte DPadDownOrButtonStart = 0x03;
+    
+    private readonly IGameBoy _gameBoy = TestGameBoyBuilder
+        .CreateBuilder()
+        .WithProcessor(processor =>
+        {
+            processor.SetInterruptMasterEnableToValue(true);
+            processor.SetJoypadInterruptEnableToValue(true);
+        })
+        .BuildGameBoy();
+    
+    [Theory]
+    [InlineData(DPadEnabled, DPadRightOrButtonA)]
+    [InlineData(DPadEnabled, DPadLeftOrButtonB)]
+    [InlineData(DPadEnabled, DPadUpOrButtonSelect)]
+    [InlineData(DPadEnabled, DPadDownOrButtonStart)]
+    [InlineData(ButtonsEnabled, DPadRightOrButtonA)]
+    [InlineData(ButtonsEnabled, DPadLeftOrButtonB)]
+    [InlineData(ButtonsEnabled, DPadUpOrButtonSelect)]
+    [InlineData(ButtonsEnabled, DPadDownOrButtonStart)]
+    public void JoypadInput_DPadOrButtonsEnabled_JoypadInterruptHandlerTriggered(byte enabler, byte joypadInput)
     {
         var cartridge = CartridgeBuilder
             .Create()
             .WithProgram([
                 Opcode.Ld_A_N8,
-                0x10,
+                enabler,
                 Opcode.Ld_HL_N16,
                 0x00,
                 0xFF,
@@ -34,24 +58,21 @@ public class JoypadInterruptTest
             ])
             .Build();
         
-        var gameBoy = TestGameBoyBuilder
-            .CreateBuilder()
-            .WithProcessor(processor =>
-            {
-                processor.SetInterruptMasterEnableToValue(true);
-                processor.SetJoypadInterruptEnableToValue(true);
-            })
-            .BuildGameBoy();
-        gameBoy.Load(cartridge);
+        _gameBoy.Load(cartridge);
         
-        gameBoy.RunFor(5);
-        gameBoy.ButtonPressed(Button.A);
-        gameBoy.RunFor(2);
-        gameBoy.ButtonReleased(Button.A);
+        _gameBoy.RunFor(5); // Run until P1 is enabled with dpad or button input
+        switch (enabler)
+        {
+            case ButtonsEnabled:
+                _gameBoy.ButtonPressed((Button)joypadInput);
+                break;
+            case DPadEnabled:
+                _gameBoy.DPadPressed((DPad)joypadInput);
+                break;
+        }
+        _gameBoy.RunFor(2);
 
-        var processor = (ITestableProcessor)gameBoy.GetProcessor();
-        var result = processor.GetValueOfRegisterA();
-        Assert.Equal(JoypadInterruptDidTriggerValue, result);
+        var actual = ((ITestableProcessor)_gameBoy.GetProcessor()).GetValueOfRegisterA();
+        Assert.Equal(JoypadInterruptDidTriggerValue, actual);
     }
-
 }
