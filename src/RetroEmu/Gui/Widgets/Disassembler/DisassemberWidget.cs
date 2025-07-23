7410dddaceb70663b1b8a8131729222e6fd72420
Henning Tandberg <henningtandberg@gmail.com>
2025-07-23 14:21:25 +0200

    set Draw to unsafe to avoid another level of nesting

--- src/RetroEmu/Gui/Widgets/Disassembler/DisassemberWidget.cs
+++ src/RetroEmu/Gui/Widgets/Disassembler/DisassemberWidget.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using ImGuiNET;
@@ -15,99 +14,96 @@ public class DisassemblerWidget(
 {
     private readonly IDisassemblerColorTheme _colorTheme = new DefaultDisassemblerColorTheme();
     
-    public void Draw(GameTime gameTime)
+    public unsafe void Draw(GameTime gameTime)
     {
-        unsafe
+        if (disassembler.DisassembledInstructions.Count <= 0)
         {
-            if (disassembler.DisassembledInstructions.Count <= 0)
-            {
-                return;
-            }
-        
-            if (!ImGui.Begin("Disassembler"))
-            {
-                return;
-            }
-        
-            ImGui.PushFont(ImGui.GetFont());
-
-            var windowSize = new Vector2(ImGui.GetContentRegionAvail().X, 0);
-            if (!ImGui.BeginChild("Instructions", windowSize, false, 0))
-            {
-                ImGui.End();
-                return;
-            }
-
-            var disassemblerSize = disassembler.DisassembledInstructions.Count + disassembler.Labels.Count + 1;
-            var clipper = new ImGuiListClipperPtr(ImGuiNative.ImGuiListClipper_ImGuiListClipper());
-            clipper.Begin(disassemblerSize, ImGui.GetTextLineHeightWithSpacing());
-            
-            var addresses = disassembler.DisassembledInstructions.Keys.ToList();
-            addresses.Sort();
-
-            var labels = new Dictionary<ushort, string>(disassembler.Labels);
-
-            while (clipper.Step())
-            {
-                var i = 0;
-                for (var item = clipper.DisplayStart; item < clipper.DisplayEnd && i < addresses.Count; item++)
-                {
-                    var address = addresses[i];
-                    if (labels.Remove(address, out var label))
-                    {
-                        ImGui.TextColored(_colorTheme.LabelColor, $"{label}:");
-                        continue;
-                    }
-
-                    var di = disassembler.DisassembledInstructions[address];
-                
-                    ImGui.PushID(item);
-                
-                    ImGui.TextColored(_colorTheme.AddressColor, $"0x{di.Address:X4}");
-
-                    var bytesString = di.Bytes.Aggregate("", (bs, b) => bs + $" {b:X2}");
-                    ImGui.SameLine();
-                    ImGui.TextColored(_colorTheme.BytesColor, $"{bytesString,-9}");
-                
-                    ImGui.SameLine();
-                    ImGui.TextColored(_colorTheme.OpcodeColor, $" {di.OpcodeToken,-4}");
-
-                    if (di.Operand1Token is not EmptyOperandToken)
-                    {
-                        ImGui.SameLine();
-                        var delimiter = ",".PadRight(7 - di.Operand1Token.Value.Length);
-                        var color = di.Operand1Token is RegisterOperandToken
-                            ? _colorTheme.RegisterOperandColor
-                            : _colorTheme.ImmediateOperandColor;
-                        ImGui.TextColored(color, $" {di.Operand1Token}{delimiter}");
-                    }
-
-                    if (di.Operand2Token is not EmptyOperandToken)
-                    {
-                        ImGui.SameLine();
-                        var color = di.Operand2Token is RegisterOperandToken
-                            ? _colorTheme.RegisterOperandColor
-                            : _colorTheme.ImmediateOperandColor;
-                        ImGui.TextColored(color, $" {di.Operand2Token,-7}");
-                    }
-
-                    if (di.IsJump() || di.IsReturn())
-                    {
-                        ImGui.PushStyleColor(ImGuiCol.Separator, _colorTheme.SeparatorColor);
-                        ImGui.Separator();
-                        ImGui.PopStyleColor();
-                    }
-
-                    ImGui.PopID();
-                    i++;
-                }
-            }
-        
-        
-            clipper.End();
-            ImGui.PopFont();
-            ImGui.EndChild();
-            ImGui.End();
+            return;
         }
+        
+        if (!ImGui.Begin("Disassembler"))
+        {
+            return;
+        }
+        
+        ImGui.PushFont(ImGui.GetFont());
+
+        var windowSize = new Vector2(ImGui.GetContentRegionAvail().X, 0);
+        if (!ImGui.BeginChild("Instructions", windowSize, false, 0))
+        {
+            ImGui.End();
+            return;
+        }
+
+        var disassemblerSize = disassembler.DisassembledInstructions.Count + disassembler.Labels.Count + 1;
+        var clipper = new ImGuiListClipperPtr(ImGuiNative.ImGuiListClipper_ImGuiListClipper());
+        clipper.Begin(disassemblerSize, ImGui.GetTextLineHeightWithSpacing());
+            
+        var addresses = disassembler.DisassembledInstructions.Keys.ToList();
+        addresses.Sort();
+
+        var labels = new Dictionary<ushort, string>(disassembler.Labels);
+
+        while (clipper.Step())
+        {
+            var i = 0;
+            for (var item = clipper.DisplayStart; item < clipper.DisplayEnd && i < addresses.Count; item++)
+            {
+                var address = addresses[i];
+                if (labels.Remove(address, out var label))
+                {
+                    ImGui.TextColored(_colorTheme.LabelColor, $"{label}:");
+                    continue;
+                }
+
+                var di = disassembler.DisassembledInstructions[address];
+                
+                ImGui.PushID(item);
+                
+                ImGui.TextColored(_colorTheme.AddressColor, $"0x{di.Address:X4}");
+
+                var bytesString = di.Bytes.Aggregate("", (bs, b) => bs + $" {b:X2}");
+                ImGui.SameLine();
+                ImGui.TextColored(_colorTheme.BytesColor, $"{bytesString,-9}");
+                
+                ImGui.SameLine();
+                ImGui.TextColored(_colorTheme.OpcodeColor, $" {di.OpcodeToken,-4}");
+
+                if (di.Operand1Token is not EmptyOperandToken)
+                {
+                    ImGui.SameLine();
+                    var delimiter = ",".PadRight(7 - di.Operand1Token.Value.Length);
+                    var color = di.Operand1Token is RegisterOperandToken
+                        ? _colorTheme.RegisterOperandColor
+                        : _colorTheme.ImmediateOperandColor;
+                    ImGui.TextColored(color, $" {di.Operand1Token}{delimiter}");
+                }
+
+                if (di.Operand2Token is not EmptyOperandToken)
+                {
+                    ImGui.SameLine();
+                    var color = di.Operand2Token is RegisterOperandToken
+                        ? _colorTheme.RegisterOperandColor
+                        : _colorTheme.ImmediateOperandColor;
+                    ImGui.TextColored(color, $" {di.Operand2Token,-7}");
+                }
+
+                if (di.IsJump() || di.IsReturn())
+                {
+                    ImGui.PushStyleColor(ImGuiCol.Separator, _colorTheme.SeparatorColor);
+                    ImGui.Separator();
+                    ImGui.PopStyleColor();
+                }
+
+                ImGui.PopID();
+                i++;
+            }
+        }
+        
+        
+        clipper.End();
+        ImGui.PopFont();
+        ImGui.EndChild();
+        ImGui.End();
     }
 }
