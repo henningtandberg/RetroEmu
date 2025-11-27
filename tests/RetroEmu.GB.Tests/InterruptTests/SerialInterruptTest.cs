5539cc905b2d7ac64c6e03f34d93825ac3fe21bd
Henning Tandberg <henningtandberg@gmail.com>
2025-11-27 19:56:59 +0100

    failing test which verifies that serial interrupt is generated correctly

--- tests/RetroEmu.GB.Tests/InterruptTests/SerialInterruptTest.cs
+++ tests/RetroEmu.GB.Tests/InterruptTests/SerialInterruptTest.cs
@@ -1,3 +1,4 @@
+using RetroEmu.Devices.DMG;
 using RetroEmu.Devices.DMG.CPU;
 using RetroEmu.GB.TestSetup;
 using Xunit;
@@ -6,53 +7,67 @@ namespace RetroEmu.GB.Tests.InterruptTests;
 
 public class SerialInterruptTest
 {
-    private const byte InterruptDidTriggerValue = 0x01;
-    private const byte InterruptDidNotTriggerValue = 0x02;
-
-    [Theory]
-    [InlineData(false, false, false, false)]
-    [InlineData(false, false, true, false)]
-    [InlineData(false, true, false, false)]
-    [InlineData(false, true, true, false)]
-    [InlineData(true, false, false, false)]
-    [InlineData(true, false, true, false)]
-    [InlineData(true, true, false, false)]
-    [InlineData(true, true, true, true)]
-    public static void InterruptProgram_SerialInterrupt_CheckIfInterruptTriggered(
-        bool IME, bool IE, bool triggerInterrupt, bool expectInterruptTriggered)
+    private const byte SerialInterruptHandlerStartAddress = 0x58;
+    
+    private readonly IGameBoy _gameBoy = TestGameBoyBuilder
+        .CreateBuilder()
+        .WithProcessor(processor =>
+        {
+            processor.SetInterruptMasterEnableToValue(true);
+            processor.SetTimerInterruptEnableToValue(true);
+        })
+        .BuildGameBoy();
+    
+    /// <summary>
+    /// This program causes 0x75 to be shifted out of the
+    /// serial port and a byte to be shifted into 0xFF01 (SB)
+    ///
+    /// In this example we assume that the internal clock is
+    /// selected this it should take:
+    ///
+    ///     1 / SERIAL_CLOCK_SPEED * 8
+    ///   = 1 / 8192 Hz * 8
+    ///   = 1 / 122 microseconds * 8
+    ///   = 976 microseconds
+    ///
+    /// But we run in an infinite loop until we are in the
+    /// serial interrupt handler which should happen after
+    /// approx 1 ms. If the handler is not triggered after
+    /// a timeout we assume the test failed.
+    /// </summary>
+    private readonly byte[] _serialInterruptProgram = CartridgeBuilder
+        .Create()
+        .WithProgram([
+            Opcode.Ld_A_N8,     // LD A, $75        # Data to transfer
+            0x75,
+            Opcode.Ld_HL_N16,   // LD HL, $FF01
+            0x01,
+            0xFF,
+            Opcode.Ld_XHL_A,    // LD ($FF01), A    # Move data to transfer to SB
+            Opcode.Ld_A_N8,     // LD A, $81
+            0x81,
+            Opcode.Ld_HL_N16,   // LD HL, $FF02
+            0x02,
+            0xFF,
+            Opcode.Ld_XHL_A,    // LD ($FF02), A    # Enable Serial transfer
+            Opcode.Jr_N8,       // Enter never ending loop
+            0xFE
+        ])
+        .WithSerialInterruptHandler([
+            Opcode.Nop // We don't need to do anything here
+        ])
+        .Build();
+    
+    [Fact]
+    public void SerialInterruptProgram_SerialIsEnabled_SerialInterruptIsTriggeredInTime()
     {
-        var gameBoy = TestGameBoyBuilder
-            .CreateBuilder()
-            .WithProcessor(processor =>
-            {
-                processor.SetInterruptMasterEnableToValue(IME);
-                processor.SetSerialInterruptEnableToValue(IE);
-            })
-            .BuildGameBoy();
-        var cartridge = CartridgeBuilder
-            .Create()
-            .WithProgram([
-                Opcode.Ld_A_N8,
-                InterruptDidNotTriggerValue,
-            ])
-            .WithSerialInterruptHandler([
-                Opcode.Ld_A_N8,
-                InterruptDidTriggerValue
-            ])
-            .Build();
-        gameBoy.Load(cartridge);
-        var processor = (ITestableProcessor)gameBoy.GetProcessor();
+        _gameBoy.Load(_serialInterruptProgram);
+        var processor = (ITestableProcessor)_gameBoy.GetProcessor();
         processor.SetProgramCounter(0x0150); // Skip program start routine at 0x0100 (NOP + JP N16)
 
-        if (triggerInterrupt)
-        {
-            processor.GenerateSerialInterrupt();
-        }
-        gameBoy.Update();
+        _gameBoy.RunWhile(() => processor.GetValueOfRegisterPC() != SerialInterruptHandlerStartAddress, maxCycles: 1200);
         
-        var result = processor.GetValueOfRegisterA();
-        Assert.Contains(result, [InterruptDidTriggerValue, InterruptDidNotTriggerValue]);
-        var didInterruptTrigger = result == InterruptDidTriggerValue;
-        Assert.Equal(didInterruptTrigger, expectInterruptTriggered);
+        var result = processor.GetValueOfRegisterPC();
+        Assert.Equal(SerialInterruptHandlerStartAddress, result);
     }
 }
