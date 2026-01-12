55dd34a748f1985c04ce01c2e0c8090f53297663
Henning Tandberg <henningtandberg@gmail.com>
2026-01-12 14:00:27 +0100

    fix JP/JR tests #33

--- tests/RetroEmu.GB.Tests/IsolatedOperationTests/JumpTests.cs
+++ tests/RetroEmu.GB.Tests/IsolatedOperationTests/JumpTests.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using RetroEmu.Devices.DMG;
 using RetroEmu.Devices.DMG.CPU;
 using RetroEmu.GB.TestSetup;
 using Xunit;
@@ -7,247 +7,78 @@ namespace RetroEmu.GB.Tests.IsolatedOperationTests;
 
 public class JumpTests
 {
-    [Fact]
-    public static void JpN16_ProgramCounterIsUpdatedCorrectlyAndCorrectCyclesReturned()
+    private readonly IGameBoy _gameBoy = TestGameBoyBuilder.CreateBuilder().BuildGameBoy();
+
+    [Theory]
+    [ClassData(typeof(JumpToAddressTestData))]
+    public void AnyJumpToAddress_ProgramCounterIsUpdatedCorrectlyCorrectCyclesReturned(
+            byte[] program, InitialState initialState, ExpectedState expectedState)
     {
-        var gameBoy = TestGameBoyBuilder
-            .CreateBuilder()
-            .WithProcessor(processor => processor.SetProgramCounter(0x0000))
-            .WithMemory(() => new Dictionary<ushort, byte>
-            {
-                [0x0000] = Opcode.Jp_N16,
-                [0x0001] = 0x34,
-                [0x0002] = 0x12
-            })
-            .BuildGameBoy();
-        
-        var cycles = gameBoy.Update();
-        
-        var processor = (ITestableProcessor)gameBoy.GetProcessor();
-        Assert.Equal(16, cycles);
-        Assert.Equal(0x1234, processor.GetValueOfRegisterPC());
+        var cartridge = CartridgeBuilder.Create().WithProgram(program).Build();
+        _gameBoy.Load(cartridge);
+        _gameBoy.SetInitialState(initialState);
+
+        var cycles = _gameBoy.Update();
+
+        _gameBoy.AssertExpectedState(expectedState);
+        Assert.Equal(expectedState.Cycles, cycles);
+    }
+
+    private class JumpToAddressTestData : TheoryData<byte[], InitialState, ExpectedState>
+    {
+        public JumpToAddressTestData()
+        {
+            Add([Opcode.Jp_N16, 0x34, 0x12], new InitialState(), new ExpectedState { Cycles = 16, PC = 0x1234 });
+            
+            Add([Opcode.JpC_N16, 0x34, 0x12], new InitialState { CarryFlag = true }, new ExpectedState { Cycles = 16, PC = 0x1234 });
+            Add([Opcode.JpC_N16, 0x34, 0x12], new InitialState { CarryFlag = false }, new ExpectedState { Cycles = 12, PC = 0x0153 });
+            Add([Opcode.JpNC_N16, 0x34, 0x12], new InitialState { CarryFlag = false }, new ExpectedState { Cycles = 16, PC = 0x1234 });
+            Add([Opcode.JpNC_N16, 0x34, 0x12], new InitialState { CarryFlag = true }, new ExpectedState { Cycles = 12, PC = 0x0153 });
+            
+            Add([Opcode.JpZ_N16, 0x34, 0x12], new InitialState { ZeroFlag = true }, new ExpectedState { Cycles = 16, PC = 0x1234 });
+            Add([Opcode.JpZ_N16, 0x34, 0x12], new InitialState { ZeroFlag = false }, new ExpectedState { Cycles = 12, PC = 0x0153 });
+            Add([Opcode.JpNZ_N16, 0x34, 0x12], new InitialState { ZeroFlag = false }, new ExpectedState { Cycles = 16, PC = 0x1234 });
+            Add([Opcode.JpNZ_N16, 0x34, 0x12], new InitialState { ZeroFlag = true }, new ExpectedState { Cycles = 12, PC = 0x0153 });
+        }
     }
     
     [Theory]
-    [InlineData(true, 0x1234, 16)]
-    [InlineData(false, 0x0003, 12)]
-    public static void JpCN16_ProgramCounterIsUpdatedCorrectlyAndCorrectCyclesReturned(bool carryFlag, ushort expectedProgramCounter, int expectedCycles)
+    [ClassData(typeof(JumpRelativeTestData))]
+    public void AnyJumpRelative_ProgramCounterIsUpdatedCorrectlyCorrectCyclesReturned(
+        byte[] program, InitialState initialState, ExpectedState expectedState)
     {
-        var gameBoy = TestGameBoyBuilder
-            .CreateBuilder()
-            .WithProcessor(processor => processor
-                .SetFlags(false, false, false, carryFlag)
-            )
-            .WithMemory(() => new Dictionary<ushort, byte>
-            {
-                [0x0000] = Opcode.JpC_N16,
-                [0x0001] = 0x34,
-                [0x0002] = 0x12
-            })
-            .BuildGameBoy();
-        
-        var cycles = gameBoy.Update();
-        
-        var processor = (ITestableProcessor)gameBoy.GetProcessor();
-        Assert.Equal(expectedCycles, cycles);
-        Assert.Equal(expectedProgramCounter, processor.GetValueOfRegisterPC());
+        var cartridge = CartridgeBuilder.Create().WithProgram(program).Build();
+        _gameBoy.Load(cartridge);
+        _gameBoy.SetInitialState(initialState);
+
+        var cycles = _gameBoy.Update();
+
+        _gameBoy.AssertExpectedState(expectedState);
+        Assert.Equal(expectedState.Cycles, cycles);
     }
-    
-    [Theory]
-    [InlineData(false, 0x1234, 16)]
-    [InlineData(true, 0x0003, 12)]
-    public static void JpNCN16_ProgramCounterIsUpdatedCorrectlyAndCorrectCyclesReturned(bool carryFlag, ushort expectedProgramCounter, int expectedCycles)
+
+    private class JumpRelativeTestData : TheoryData<byte[], InitialState, ExpectedState>
     {
-        var gameBoy = TestGameBoyBuilder
-            .CreateBuilder()
-            .WithProcessor(processor => processor
-                .SetFlags(false, false, false, carryFlag)
-            )
-            .WithMemory(() => new Dictionary<ushort, byte>
-            {
-                [0x0000] = Opcode.JpNC_N16,
-                [0x0001] = 0x34,
-                [0x0002] = 0x12
-            })
-            .BuildGameBoy();
-        
-        var cycles = gameBoy.Update();
-        
-        var processor = (ITestableProcessor)gameBoy.GetProcessor();
-        Assert.Equal(expectedCycles, cycles);
-        Assert.Equal(expectedProgramCounter, processor.GetValueOfRegisterPC());
-    }
-    
-    [Theory]
-    [InlineData(true, 0x1234, 16)]
-    [InlineData(false, 0x0003, 12)]
-    public static void JpZN16_ProgramCounterIsUpdatedCorrectlyAndCorrectCyclesReturned(bool zeroFlag, ushort expectedProgramCounter, int expectedCycles)
-    {
-        var gameBoy = TestGameBoyBuilder
-            .CreateBuilder()
-            .WithProcessor(processor => processor
-                .SetFlags(zeroFlag, false, false, false)
-            )
-            .WithMemory(() => new Dictionary<ushort, byte>
-            {
-                [0x0000] = Opcode.JpZ_N16,
-                [0x0001] = 0x34,
-                [0x0002] = 0x12
-            })
-            .BuildGameBoy();
-        
-        var cycles = gameBoy.Update();
-        
-        var processor = (ITestableProcessor)gameBoy.GetProcessor();
-        Assert.Equal(expectedCycles, cycles);
-        Assert.Equal(expectedProgramCounter, processor.GetValueOfRegisterPC());
-    }
-    
-    [Theory]
-    [InlineData(false, 0x1234, 16)]
-    [InlineData(true, 0x0003, 12)]
-    public static void JpNZN16_ProgramCounterIsUpdatedCorrectlyAndCorrectCyclesReturned(bool zeroFlag, ushort expectedProgramCounter, int expectedCycles)
-    {
-        var gameBoy = TestGameBoyBuilder
-            .CreateBuilder()
-            .WithProcessor(processor => processor
-                .SetFlags(zeroFlag, false, false, false)
-            )
-            .WithMemory(() => new Dictionary<ushort, byte>
-            {
-                [0x0000] = Opcode.JpNZ_N16,
-                [0x0001] = 0x34,
-                [0x0002] = 0x12
-            })
-            .BuildGameBoy();
-        
-        var cycles = gameBoy.Update();
-        
-        var processor = (ITestableProcessor)gameBoy.GetProcessor();
-        Assert.Equal(expectedCycles, cycles);
-        Assert.Equal(expectedProgramCounter, processor.GetValueOfRegisterPC());
-    }
-    
-    [Theory]
-    [InlineData(-1, 0x0001)]
-    [InlineData(1, 0x0003)]
-    public static void JrN8_ProgramCounterIsUpdatedCorrectlyAndCorrectCyclesReturned(sbyte relativeJump, ushort expectedProgramCounter)
-    {
-        var gameBoy = TestGameBoyBuilder
-            .CreateBuilder()
-            .WithProcessor(processor =>
-                processor.SetProgramCounter(0x0000))
-            .WithMemory(() => new Dictionary<ushort, byte>
-            {
-                [0x0000] = Opcode.Jr_N8,
-                [0x0001] = (byte)relativeJump
-            })
-            .BuildGameBoy();
-        
-        var cycles = gameBoy.Update();
-        
-        var processor = (ITestableProcessor)gameBoy.GetProcessor();
-        Assert.Equal(12, cycles);
-        Assert.Equal(expectedProgramCounter, processor.GetValueOfRegisterPC());
-    }
-    
-    [Theory]
-    [InlineData(true, -1,0x0001, 12)]
-    [InlineData(true, 1,0x0003, 12)]
-    [InlineData(false, 1, 0x0002, 8)]
-    public static void JrCN8_ProgramCounterIsUpdatedCorrectlyAndCorrectCyclesReturned(bool carryFlag, sbyte relativeJump, ushort expectedProgramCounter, int expectedCycles)
-    {
-        var gameBoy = TestGameBoyBuilder
-            .CreateBuilder()
-            .WithProcessor(processor => processor
-                .SetFlags(false, false, false, carryFlag)
-            )
-            .WithMemory(() => new Dictionary<ushort, byte>
-            {
-                [0x0000] = Opcode.JrC_N8,
-                [0x0001] = (byte)relativeJump,
-            })
-            .BuildGameBoy();
-        
-        var cycles = gameBoy.Update();
-        
-        var processor = (ITestableProcessor)gameBoy.GetProcessor();
-        Assert.Equal(expectedCycles, cycles);
-        Assert.Equal(expectedProgramCounter, processor.GetValueOfRegisterPC());
-    }
-    
-    [Theory]
-    [InlineData(false, -1,0x0001, 12)]
-    [InlineData(false, 1,0x0003, 12)]
-    [InlineData(true, 1, 0x0002, 8)]
-    public static void JrNCN8_ProgramCounterIsUpdatedCorrectlyAndCorrectCyclesReturned(bool carryFlag, sbyte relativeJump, ushort expectedProgramCounter, int expectedCycles)
-    {
-        var gameBoy = TestGameBoyBuilder
-            .CreateBuilder()
-            .WithProcessor(processor => processor
-                .SetFlags(false, false, false, carryFlag)
-            )
-            .WithMemory(() => new Dictionary<ushort, byte>
-            {
-                [0x0000] = Opcode.JrNC_N8,
-                [0x0001] = (byte)relativeJump
-            })
-            .BuildGameBoy();
-        
-        var cycles = gameBoy.Update();
-        
-        var processor = (ITestableProcessor)gameBoy.GetProcessor();
-        Assert.Equal(expectedCycles, cycles);
-        Assert.Equal(expectedProgramCounter, processor.GetValueOfRegisterPC());
-    }
-    
-    [Theory]
-    [InlineData(true, -1,0x0001, 12)]
-    [InlineData(true, 1,0x0003, 12)]
-    [InlineData(false, 1, 0x0002, 8)]
-    public static void JrZN8_ProgramCounterIsUpdatedCorrectlyAndCorrectCyclesReturned(bool zeroFlag, sbyte relativeJump, ushort expectedProgramCounter, int expectedCycles)
-    {
-        var gameBoy = TestGameBoyBuilder
-            .CreateBuilder()
-            .WithProcessor(processor => processor
-                .SetFlags(zeroFlag, false, false, false)
-            )
-            .WithMemory(() => new Dictionary<ushort, byte>
-            {
-                [0x0000] = Opcode.JrZ_N8,
-                [0x0001] = (byte)relativeJump
-            })
-            .BuildGameBoy();
-        
-        var cycles = gameBoy.Update();
-        
-        var processor = (ITestableProcessor)gameBoy.GetProcessor();
-        Assert.Equal(expectedCycles, cycles);
-        Assert.Equal(expectedProgramCounter, processor.GetValueOfRegisterPC());
-    }
-    
-    [Theory]
-    [InlineData(false, -1,0x0001, 12)]
-    [InlineData(false, 1,0x0003, 12)]
-    [InlineData(true, 1, 0x0002, 8)]
-    public static void JrNZN8_ProgramCounterIsUpdatedCorrectlyAndCorrectCyclesReturned(bool zeroFlag, sbyte relativeJump, ushort expectedProgramCounter, int expectedCycles)
-    {
-        var gameBoy = TestGameBoyBuilder
-            .CreateBuilder()
-            .WithProcessor(processor => processor
-                .SetFlags(zeroFlag, false, false, false)
-            )
-            .WithMemory(() => new Dictionary<ushort, byte>
-            {
-                [0x0000] = Opcode.JrNZ_N8,
-                [0x0001] = (byte)relativeJump
-            })
-            .BuildGameBoy();
-        
-        var cycles = gameBoy.Update();
-        
-        var processor = (ITestableProcessor)gameBoy.GetProcessor();
-        Assert.Equal(expectedCycles, cycles);
-        Assert.Equal(expectedProgramCounter, processor.GetValueOfRegisterPC());
+        public JumpRelativeTestData()
+        {
+            Add([Opcode.Jr_N8, 0xFE], new InitialState(), new ExpectedState { Cycles = 12, PC = 0x0150 });
+            Add([Opcode.Jr_N8, 0x02], new InitialState(), new ExpectedState { Cycles = 12, PC = 0x0154 });
+            
+            Add([Opcode.JrC_N8, 0xFE], new InitialState { CarryFlag = true }, new ExpectedState { Cycles = 12, PC = 0x0150 });
+            Add([Opcode.JrC_N8, 0x02], new InitialState { CarryFlag = true }, new ExpectedState { Cycles = 12, PC = 0x0154 });
+            Add([Opcode.JrC_N8, 0xFE], new InitialState { CarryFlag = false }, new ExpectedState { Cycles = 8, PC = 0x0154 });
+            
+            Add([Opcode.JrNC_N8, 0xFE], new InitialState { CarryFlag = false }, new ExpectedState { Cycles = 12, PC = 0x0150 });
+            Add([Opcode.JrNC_N8, 0x02], new InitialState { CarryFlag = false }, new ExpectedState { Cycles = 12, PC = 0x0154 });
+            Add([Opcode.JrNC_N8, 0xFE], new InitialState { CarryFlag = true }, new ExpectedState { Cycles = 8, PC = 0x0154 });
+            
+            Add([Opcode.JrZ_N8, 0xFE], new InitialState { ZeroFlag = true }, new ExpectedState { Cycles = 12, PC = 0x0150 });
+            Add([Opcode.JrZ_N8, 0x02], new InitialState { ZeroFlag = true }, new ExpectedState { Cycles = 12, PC = 0x0154 });
+            Add([Opcode.JrZ_N8, 0xFE], new InitialState { ZeroFlag = false }, new ExpectedState { Cycles = 8, PC = 0x0154 });
+            
+            Add([Opcode.JrNZ_N8, 0xFE], new InitialState { ZeroFlag = false }, new ExpectedState { Cycles = 12, PC = 0x0150 });
+            Add([Opcode.JrNZ_N8, 0x02], new InitialState { ZeroFlag = false }, new ExpectedState { Cycles = 12, PC = 0x0154 });
+            Add([Opcode.JrNZ_N8, 0xFE], new InitialState { ZeroFlag = true }, new ExpectedState { Cycles = 8, PC = 0x0154 });
+        }
     }
 }
