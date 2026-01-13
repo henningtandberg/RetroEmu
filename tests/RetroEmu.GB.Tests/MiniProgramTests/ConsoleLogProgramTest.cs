5b5c40a8a9377db6f0c49a727731d61b98e58870
Henning Tandberg <henningtandberg@gmail.com>
2026-01-13 13:15:55 +0100

    fix console write program

--- tests/RetroEmu.GB.Tests/MiniProgramTests/ConsoleLogProgramTest.cs
+++ tests/RetroEmu.GB.Tests/MiniProgramTests/ConsoleLogProgramTest.cs
@@ -1,4 +1,5 @@
-﻿using RetroEmu.Devices.DMG;
+﻿using System.Text;
+using RetroEmu.Devices.DMG;
 using RetroEmu.Devices.DMG.CPU;
 using RetroEmu.GB.TestSetup;
 using Xunit;
@@ -7,8 +8,11 @@ namespace RetroEmu.GB.Tests.MiniProgramTests;
 
 public class ConsoleLogProgramTest
 {
+    private static readonly WireFake WireFake = new();
+    
     private readonly IGameBoy _gameBoy = TestGameBoyBuilder
         .CreateBuilder()
+        .WithWireFake(WireFake)
         .BuildGameBoy();
     
     private readonly byte[] _consoleLogHelloCartridge = CartridgeBuilder
@@ -24,6 +28,14 @@ public class ConsoleLogProgramTest
             Opcode.Ld_XN16_A,
             0x02,
             0xFF,
+            Opcode.Ld_A_XN16,
+            0x02,
+            0xFF,
+            Opcode.Pre_CB,
+            CBOpcode.Bit7_A,
+            Opcode.JrNZ_N8,
+            0xF9,
+            
             Opcode.Ld_A_N8,
             (byte)'E',
             Opcode.Ld_XN16_A,
@@ -34,6 +46,14 @@ public class ConsoleLogProgramTest
             Opcode.Ld_XN16_A,
             0x02,
             0xFF,
+            Opcode.Ld_A_XN16,
+            0x02,
+            0xFF,
+            Opcode.Pre_CB,
+            CBOpcode.Bit7_A,
+            Opcode.JrNZ_N8,
+            0xF9,
+            
             Opcode.Ld_A_N8,
             (byte)'L',
             Opcode.Ld_XN16_A,
@@ -44,6 +64,14 @@ public class ConsoleLogProgramTest
             Opcode.Ld_XN16_A,
             0x02,
             0xFF,
+            Opcode.Ld_A_XN16,
+            0x02,
+            0xFF,
+            Opcode.Pre_CB,
+            CBOpcode.Bit7_A,
+            Opcode.JrNZ_N8,
+            0xF9,
+            
             Opcode.Ld_A_N8,
             (byte)'L',
             Opcode.Ld_XN16_A,
@@ -54,6 +82,14 @@ public class ConsoleLogProgramTest
             Opcode.Ld_XN16_A,
             0x02,
             0xFF,
+            Opcode.Ld_A_XN16,
+            0x02,
+            0xFF,
+            Opcode.Pre_CB,
+            CBOpcode.Bit7_A,
+            Opcode.JrNZ_N8,
+            0xF9,
+            
             Opcode.Ld_A_N8,
             (byte)'O',
             Opcode.Ld_XN16_A,
@@ -64,6 +100,19 @@ public class ConsoleLogProgramTest
             Opcode.Ld_XN16_A,
             0x02,
             0xFF,
+            Opcode.Ld_A_XN16,
+            0x02,
+            0xFF,
+            Opcode.Pre_CB,
+            CBOpcode.Bit7_A,
+            Opcode.JrNZ_N8,
+            0xF9,
+            
+            Opcode.Ld_BC_N16,   // Set BC to 1337 to indicate finished program
+            0x39,
+            0x05,
+            Opcode.Jr_N8,       // Spin forever
+            0xFE
         ])
         .Build();
     
@@ -72,10 +121,10 @@ public class ConsoleLogProgramTest
     {
         _gameBoy.Load(_consoleLogHelloCartridge);
         var processor = (ITestableProcessor)_gameBoy.GetProcessor();
-        processor.SetProgramCounter(0x0150); // Skip program start routine at 0x0100 (NOP + JP N16)
         
-        _gameBoy.RunWhile(() => processor.GetValueOfRegisterPC() < (0x0150 + 0x32));
+        _gameBoy.RunWhile(() => processor.GetValueOfRegisterBC() != 1337); // 1337 == 0x0539
         
-        Assert.Equal("HELLO", _gameBoy.GetOutput());
+        var actualOutput = WireFake.AllOutgoingData();
+        Assert.Equal("HELLO", Encoding.ASCII.GetString(actualOutput));
     }
 }
