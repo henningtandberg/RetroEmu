11be91721eca0ea71f653f5ffc84018e514c5729
Henning Tandberg <henningtandberg@gmail.com>
2026-01-13 13:13:50 +0100

    fix CB Shift test #41

--- tests/RetroEmu.GB.Tests/IsolatedOperationTests/CbShiftTests.cs
+++ tests/RetroEmu.GB.Tests/IsolatedOperationTests/CbShiftTests.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Collections.Generic;
+using RetroEmu.Devices.DMG;
 using RetroEmu.Devices.DMG.CPU;
 using RetroEmu.GB.TestSetup;
 using Xunit;
@@ -8,111 +7,140 @@ namespace RetroEmu.GB.Tests.IsolatedOperationTests;
 
 public class CbShiftTests
 {
+    private readonly IGameBoy _gameBoy = TestGameBoyBuilder.CreateBuilder().BuildGameBoy();
+    
     [Theory]
-    [InlineData(ProgramToRun.SLA, 0b00001111, 56, 0b00011110, false, false)]
-    [InlineData(ProgramToRun.SLA, 0b11110000, 56, 0b11100000, true, false)]
-    [InlineData(ProgramToRun.SLA, 0b00000000, 56, 0b00000000, false, true)]
-    [InlineData(ProgramToRun.SRA, 0b00001111, 56, 0b00000111, true, false)]
-    [InlineData(ProgramToRun.SRA, 0b11110000, 56, 0b11111000, false, false)]
-    [InlineData(ProgramToRun.SRA, 0b00000000, 56, 0b00000000, false, true)]
-    [InlineData(ProgramToRun.SRL, 0b00001111, 56, 0b00000111, true, false)]
-    [InlineData(ProgramToRun.SRL, 0b11110000, 56, 0b01111000, false, false)]
-    [InlineData(ProgramToRun.SRL, 0b00000000, 56, 0b00000000, false, true)]
-    public static void CBShiftRight8BitRegisterProgram_ResultCyclesCarryAndZeroIsSetExpected(
-            ProgramToRun programToRun, byte input, int expectedCycles, byte expectedResult, bool expectedCarry, bool expectedZero)
+    [ClassData(typeof(SLATestData))]
+    public void CBOperation_SLA_ResultCyclesCarryAndZeroIsSetExpected(
+        byte[] program, InitialState initialState, ExpectedState expectedState)
     {
-        var program = GetRotateProgram(programToRun);
-        var gameBoy = TestGameBoyBuilder
-            .CreateBuilder()
-            .WithProcessor(processor => processor
-                .Set8BitGeneralPurposeRegisters(a: input, b: input, c: input, d: input, e: input, h: input, l: input)
-                .SetProgramCounter(0x0001)
-            )
-            .WithMemory(() => program)
-            .BuildGameBoy();
+        var cartridge = CartridgeBuilder.Create().WithProgram(program).Build();
+        _gameBoy.Load(cartridge);
+        _gameBoy.SetInitialState(initialState);
 
-        var cycles = 0;
-        var processor = (ITestableProcessor)gameBoy.GetProcessor();
-        while (processor.GetValueOfRegisterPC() < program.Keys.Count)
+        var cycles = _gameBoy.Update();
+
+        _gameBoy.AssertExpectedState(expectedState);
+        Assert.Equal(expectedState.Cycles, cycles);
+    }
+
+    private class SLATestData : TheoryData<byte[], InitialState, ExpectedState>
+    {
+        public SLATestData()
         {
-            cycles += gameBoy.Update();
+            Add([Opcode.Pre_CB, CBOpcode.Sla_A], new InitialState { A = 0b00001111 }, new ExpectedState { Cycles = 8, A = 0b00011110, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_B], new InitialState { B = 0b00001111 }, new ExpectedState { Cycles = 8, B = 0b00011110, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_C], new InitialState { C = 0b00001111 }, new ExpectedState { Cycles = 8, C = 0b00011110, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_D], new InitialState { D = 0b00001111 }, new ExpectedState { Cycles = 8, D = 0b00011110, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_E], new InitialState { E = 0b00001111 }, new ExpectedState { Cycles = 8, E = 0b00011110, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_H], new InitialState { H = 0b00001111 }, new ExpectedState { Cycles = 8, H = 0b00011110, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_L], new InitialState { L = 0b00001111 }, new ExpectedState { Cycles = 8, L = 0b00011110, CarryFlag = false, ZeroFlag = false });
             
-            Assert.Equal(expectedCarry, processor.GetValueOfCarryFlag());
-            Assert.False(processor.GetValueOfHalfCarryFlag());
-            Assert.False(processor.GetValueOfSubtractFlag());
-            Assert.Equal(expectedZero, processor.GetValueOfZeroFlag());
+            Add([Opcode.Pre_CB, CBOpcode.Sla_A], new InitialState { A = 0b11110000 }, new ExpectedState { Cycles = 8, A = 0b11100000, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_B], new InitialState { B = 0b11110000 }, new ExpectedState { Cycles = 8, B = 0b11100000, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_C], new InitialState { C = 0b11110000 }, new ExpectedState { Cycles = 8, C = 0b11100000, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_D], new InitialState { D = 0b11110000 }, new ExpectedState { Cycles = 8, D = 0b11100000, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_E], new InitialState { E = 0b11110000 }, new ExpectedState { Cycles = 8, E = 0b11100000, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_H], new InitialState { H = 0b11110000 }, new ExpectedState { Cycles = 8, H = 0b11100000, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_L], new InitialState { L = 0b11110000 }, new ExpectedState { Cycles = 8, L = 0b11100000, CarryFlag = true, ZeroFlag = false });
+            
+            Add([Opcode.Pre_CB, CBOpcode.Sla_A], new InitialState { A = 0b00000000 }, new ExpectedState { Cycles = 8, A = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_B], new InitialState { B = 0b00000000 }, new ExpectedState { Cycles = 8, B = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_C], new InitialState { C = 0b00000000 }, new ExpectedState { Cycles = 8, C = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_D], new InitialState { D = 0b00000000 }, new ExpectedState { Cycles = 8, D = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_E], new InitialState { E = 0b00000000 }, new ExpectedState { Cycles = 8, E = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_H], new InitialState { H = 0b00000000 }, new ExpectedState { Cycles = 8, H = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Sla_L], new InitialState { L = 0b00000000 }, new ExpectedState { Cycles = 8, L = 0b00000000, CarryFlag = false, ZeroFlag = true });
         }
+    }
+    
+    [Theory]
+    [ClassData(typeof(SRATestData))]
+    public void CBOperation_SRA_ResultCyclesCarryAndZeroIsSetExpected(
+        byte[] program, InitialState initialState, ExpectedState expectedState)
+    {
+        var cartridge = CartridgeBuilder.Create().WithProgram(program).Build();
+        _gameBoy.Load(cartridge);
+        _gameBoy.SetInitialState(initialState);
 
-        Assert.Equal(expectedCycles, cycles);
-        Assert.Equal(expectedResult, processor.GetValueOfRegisterA());
-        Assert.Equal(expectedResult, processor.GetValueOfRegisterB());
-        Assert.Equal(expectedResult, processor.GetValueOfRegisterC());
-        Assert.Equal(expectedResult, processor.GetValueOfRegisterD());
-        Assert.Equal(expectedResult, processor.GetValueOfRegisterE());
-        Assert.Equal(expectedResult, processor.GetValueOfRegisterH());
-        Assert.Equal(expectedResult, processor.GetValueOfRegisterL());
+        var cycles = _gameBoy.Update();
+
+        _gameBoy.AssertExpectedState(expectedState);
+        Assert.Equal(expectedState.Cycles, cycles);
     }
 
-    public enum ProgramToRun
+    private class SRATestData : TheoryData<byte[], InitialState, ExpectedState>
     {
-        SLA = 0,
-        SRA = 1,
-        SRL = 2,
+        public SRATestData()
+        {
+            Add([Opcode.Pre_CB, CBOpcode.Sra_A], new InitialState { A = 0b00001111 }, new ExpectedState { Cycles = 8, A = 0b00000111, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_B], new InitialState { B = 0b00001111 }, new ExpectedState { Cycles = 8, B = 0b00000111, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_C], new InitialState { C = 0b00001111 }, new ExpectedState { Cycles = 8, C = 0b00000111, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_D], new InitialState { D = 0b00001111 }, new ExpectedState { Cycles = 8, D = 0b00000111, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_E], new InitialState { E = 0b00001111 }, new ExpectedState { Cycles = 8, E = 0b00000111, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_H], new InitialState { H = 0b00001111 }, new ExpectedState { Cycles = 8, H = 0b00000111, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_L], new InitialState { L = 0b00001111 }, new ExpectedState { Cycles = 8, L = 0b00000111, CarryFlag = true, ZeroFlag = false });
+            
+            Add([Opcode.Pre_CB, CBOpcode.Sra_A], new InitialState { A = 0b11110000 }, new ExpectedState { Cycles = 8, A = 0b11111000, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_B], new InitialState { B = 0b11110000 }, new ExpectedState { Cycles = 8, B = 0b11111000, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_C], new InitialState { C = 0b11110000 }, new ExpectedState { Cycles = 8, C = 0b11111000, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_D], new InitialState { D = 0b11110000 }, new ExpectedState { Cycles = 8, D = 0b11111000, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_E], new InitialState { E = 0b11110000 }, new ExpectedState { Cycles = 8, E = 0b11111000, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_H], new InitialState { H = 0b11110000 }, new ExpectedState { Cycles = 8, H = 0b11111000, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_L], new InitialState { L = 0b11110000 }, new ExpectedState { Cycles = 8, L = 0b11111000, CarryFlag = false, ZeroFlag = false });
+            
+            Add([Opcode.Pre_CB, CBOpcode.Sra_A], new InitialState { A = 0b00000000 }, new ExpectedState { Cycles = 8, A = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_B], new InitialState { B = 0b00000000 }, new ExpectedState { Cycles = 8, B = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_C], new InitialState { C = 0b00000000 }, new ExpectedState { Cycles = 8, C = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_D], new InitialState { D = 0b00000000 }, new ExpectedState { Cycles = 8, D = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_E], new InitialState { E = 0b00000000 }, new ExpectedState { Cycles = 8, E = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_H], new InitialState { H = 0b00000000 }, new ExpectedState { Cycles = 8, H = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Sra_L], new InitialState { L = 0b00000000 }, new ExpectedState { Cycles = 8, L = 0b00000000, CarryFlag = false, ZeroFlag = true });
+        }
+    }
+    
+    [Theory]
+    [ClassData(typeof(SRLTestData))]
+    public void CBOperation_SRL_ResultCyclesCarryAndZeroIsSetExpected(
+        byte[] program, InitialState initialState, ExpectedState expectedState)
+    {
+        var cartridge = CartridgeBuilder.Create().WithProgram(program).Build();
+        _gameBoy.Load(cartridge);
+        _gameBoy.SetInitialState(initialState);
+
+        var cycles = _gameBoy.Update();
+
+        _gameBoy.AssertExpectedState(expectedState);
+        Assert.Equal(expectedState.Cycles, cycles);
     }
 
-    private static IDictionary<ushort, byte> GetRotateProgram(ProgramToRun programToRun) => programToRun switch
+    private class SRLTestData : TheoryData<byte[], InitialState, ExpectedState>
     {
-        ProgramToRun.SLA => new Dictionary<ushort, byte>
+        public SRLTestData()
         {
-            [0x0001] = Opcode.Pre_CB,
-            [0x0002] = CBOpcode.Sla_A,
-            [0x0003] = Opcode.Pre_CB,
-            [0x0004] = CBOpcode.Sla_B,
-            [0x0005] = Opcode.Pre_CB,
-            [0x0006] = CBOpcode.Sla_C,
-            [0x0007] = Opcode.Pre_CB,
-            [0x0008] = CBOpcode.Sla_D,
-            [0x0009] = Opcode.Pre_CB,
-            [0x000A] = CBOpcode.Sla_E,
-            [0x000B] = Opcode.Pre_CB,
-            [0x000C] = CBOpcode.Sla_H,
-            [0x000D] = Opcode.Pre_CB,
-            [0x000E] = CBOpcode.Sla_L
-        },
-        ProgramToRun.SRA => new Dictionary<ushort, byte>
-        {
-            [0x0001] = Opcode.Pre_CB,
-            [0x0002] = CBOpcode.Sra_A,
-            [0x0003] = Opcode.Pre_CB,
-            [0x0004] = CBOpcode.Sra_B,
-            [0x0005] = Opcode.Pre_CB,
-            [0x0006] = CBOpcode.Sra_C,
-            [0x0007] = Opcode.Pre_CB,
-            [0x0008] = CBOpcode.Sra_D,
-            [0x0009] = Opcode.Pre_CB,
-            [0x000A] = CBOpcode.Sra_E,
-            [0x000B] = Opcode.Pre_CB,
-            [0x000C] = CBOpcode.Sra_H,
-            [0x000D] = Opcode.Pre_CB,
-            [0x000E] = CBOpcode.Sra_L
-        },
-        ProgramToRun.SRL => new Dictionary<ushort, byte>
-        {
-            [0x0001] = Opcode.Pre_CB,
-            [0x0002] = CBOpcode.Srl_A,
-            [0x0003] = Opcode.Pre_CB,
-            [0x0004] = CBOpcode.Srl_B,
-            [0x0005] = Opcode.Pre_CB,
-            [0x0006] = CBOpcode.Srl_C,
-            [0x0007] = Opcode.Pre_CB,
-            [0x0008] = CBOpcode.Srl_D,
-            [0x0009] = Opcode.Pre_CB,
-            [0x000A] = CBOpcode.Srl_E,
-            [0x000B] = Opcode.Pre_CB,
-            [0x000C] = CBOpcode.Srl_H,
-            [0x000D] = Opcode.Pre_CB,
-            [0x000E] = CBOpcode.Srl_L
-        },
-        _ => throw new NotImplementedException()
-    };
+            Add([Opcode.Pre_CB, CBOpcode.Srl_A], new InitialState { A = 0b00001111 }, new ExpectedState { Cycles = 8, A = 0b00000111, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_B], new InitialState { B = 0b00001111 }, new ExpectedState { Cycles = 8, B = 0b00000111, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_C], new InitialState { C = 0b00001111 }, new ExpectedState { Cycles = 8, C = 0b00000111, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_D], new InitialState { D = 0b00001111 }, new ExpectedState { Cycles = 8, D = 0b00000111, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_E], new InitialState { E = 0b00001111 }, new ExpectedState { Cycles = 8, E = 0b00000111, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_H], new InitialState { H = 0b00001111 }, new ExpectedState { Cycles = 8, H = 0b00000111, CarryFlag = true, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_L], new InitialState { L = 0b00001111 }, new ExpectedState { Cycles = 8, L = 0b00000111, CarryFlag = true, ZeroFlag = false });
+            
+            Add([Opcode.Pre_CB, CBOpcode.Srl_A], new InitialState { A = 0b11110000 }, new ExpectedState { Cycles = 8, A = 0b01111000, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_B], new InitialState { B = 0b11110000 }, new ExpectedState { Cycles = 8, B = 0b01111000, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_C], new InitialState { C = 0b11110000 }, new ExpectedState { Cycles = 8, C = 0b01111000, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_D], new InitialState { D = 0b11110000 }, new ExpectedState { Cycles = 8, D = 0b01111000, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_E], new InitialState { E = 0b11110000 }, new ExpectedState { Cycles = 8, E = 0b01111000, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_H], new InitialState { H = 0b11110000 }, new ExpectedState { Cycles = 8, H = 0b01111000, CarryFlag = false, ZeroFlag = false });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_L], new InitialState { L = 0b11110000 }, new ExpectedState { Cycles = 8, L = 0b01111000, CarryFlag = false, ZeroFlag = false });
+            
+            Add([Opcode.Pre_CB, CBOpcode.Srl_A], new InitialState { A = 0b00000000 }, new ExpectedState { Cycles = 8, A = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_B], new InitialState { B = 0b00000000 }, new ExpectedState { Cycles = 8, B = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_C], new InitialState { C = 0b00000000 }, new ExpectedState { Cycles = 8, C = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_D], new InitialState { D = 0b00000000 }, new ExpectedState { Cycles = 8, D = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_E], new InitialState { E = 0b00000000 }, new ExpectedState { Cycles = 8, E = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_H], new InitialState { H = 0b00000000 }, new ExpectedState { Cycles = 8, H = 0b00000000, CarryFlag = false, ZeroFlag = true });
+            Add([Opcode.Pre_CB, CBOpcode.Srl_L], new InitialState { L = 0b00000000 }, new ExpectedState { Cycles = 8, L = 0b00000000, CarryFlag = false, ZeroFlag = true });
+        }
+    }
 }
